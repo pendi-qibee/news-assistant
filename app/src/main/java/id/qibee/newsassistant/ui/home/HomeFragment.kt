d4f7ae0611a8ef70bb64c0470e993b166f2372d8
pendi-qibee <pendimadyana@gmail.com>
2020-02-02 16:13:55 +0700

    change default view model initialization

--- app/src/main/java/id/qibee/newsassistant/ui/home/HomeFragment.kt
+++ app/src/main/java/id/qibee/newsassistant/ui/home/HomeFragment.kt
@@ -6,21 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import id.qibee.newsassistant.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val homeViewModel : HomeViewModel by viewModels()
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(this, Observer {
