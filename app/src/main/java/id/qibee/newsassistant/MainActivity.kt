96a35b3b36a76803c9e6e8e49b8b7b3ecd1bfdf4
pendi-qibee <pendimadyana@gmail.com>
2020-02-03 20:36:40 +0700

    tes commit

--- app/src/main/java/id/qibee/newsassistant/MainActivity.kt
+++ app/src/main/java/id/qibee/newsassistant/MainActivity.kt
@@ -25,5 +25,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
+     }
+
 }
