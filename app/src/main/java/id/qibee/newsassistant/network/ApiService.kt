b01bb6b5e69a7485107d2af3c13dad98b9440b45
pendi-qibee <pendimadyana@gmail.com>
2020-02-09 07:58:56 +0700

    rename function to getTopHeadlines

--- app/src/main/java/id/qibee/newsassistant/network/ApiService.kt
+++ app/src/main/java/id/qibee/newsassistant/network/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("top-headlines")
-    fun getPlaylist(
+    fun getTopHeadlines(
         @Query("country") country: String,
         @Query("apiKey") apiKey: String
     ): Deferred<TopHeadlines>
