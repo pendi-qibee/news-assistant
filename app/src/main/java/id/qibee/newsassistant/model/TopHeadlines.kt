c4830453acc32d0f490fc4b69e3c46d62bdf8fa4
pendi-qibee <pendimadyana@gmail.com>
2020-02-11 08:57:11 +0700

    remove null initialization

--- app/src/main/java/id/qibee/newsassistant/model/TopHeadlines.kt
+++ app/src/main/java/id/qibee/newsassistant/model/TopHeadlines.kt
@@ -8,7 +8,7 @@ data class TopHeadlines(
     val totalResults: Int? = null,
 
     @field:SerializedName("articles")
-    val articles: List<ArticlesItem?>? = null,
+    val articles: List<ArticlesItem>,
 
     @field:SerializedName("status")
     val status: String? = null
