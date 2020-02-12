b66f926c2ad6bf914158a83128712b0ecbc730ac
pendi-qibee <pendimadyana@gmail.com>
2020-02-12 19:10:56 +0700

    add menu Trending and Subscriptions

--- app/src/main/java/id/qibee/newsassistant/MainActivity.kt
+++ app/src/main/java/id/qibee/newsassistant/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_trending, R.id.navigation_subscriptions
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
